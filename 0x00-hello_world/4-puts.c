13745ef115bc2360efe9e020937b91edfbd52d97
agunbiadepaul <agunbiade_paul@yahoo.com>
2023-08-10 18:54:10 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: "Programming is like building a multilingual puzzle,"
+ *              followed by a new line.
+ * Return: Always 0 (to indicate successful completion)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle,");
+	return (0);
+}
