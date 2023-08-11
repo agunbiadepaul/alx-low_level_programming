5a30ae9e056e6fcec3bb09679cf391c8595ea807
agunbiadepaul <agunbiade_paul@yahoo.com>
2023-08-10 19:13:44 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the proper grammar, creating a piece of art.
+ *              Followed by a new line.
+ *
+ * Return: Always 0 (indicating successful completion)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
