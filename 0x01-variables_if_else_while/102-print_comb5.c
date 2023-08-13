b0f43bf24e3ebe01c9652ac68293eb1f89b98188
agunbiadepaul <agunbiade_paul@yahoo.com>
2023-08-12 19:33:59 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,12 +16,11 @@ int main(void)
 
 	for (num1 = 0; num1 < 100; num1++)
 	{
-		for (num2 = 0; num2 < 100; num2++)
+		for (num2 = num1 + 1; num2 < 100; num2++)
 		{
-
 			printf("%02d %02d", num1, num2);
 
-			if (num1 < 99 || num2 < 99)
+			if (num1 < 98 || num2 < 99)
 			{
 				putchar(',');
 				putchar(' ');
