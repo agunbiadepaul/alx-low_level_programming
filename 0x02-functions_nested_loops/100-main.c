4d5211bba8d78d051e38365aa76c53aa3f1f4103
agunbiadepaul <agunbiade_paul@yahoo.com>
2023-08-15 21:17:43 -0700

    100-times_table

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Calls the print_times_table
+ * function to print the 15 times table.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_times_table(15);
+	return (0);
+}
