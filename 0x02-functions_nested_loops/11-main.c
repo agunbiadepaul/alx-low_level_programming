888036a23c1ae12a2a8414e2340fdf8bd76e95c8
agunbiadepaul <agunbiade_paul@yahoo.com>
2023-08-15 20:39:24 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_to_98(0);
+	print_to_98(10);
+	print_to_98(98);
+	print_to_98(111);
+	return (0);
+}
