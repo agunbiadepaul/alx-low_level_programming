02360091a8b48295dcb711e4e4b101aa9241a01d
agunbiadepaul <agunbiade_paul@yahoo.com>
2023-08-22 12:40:30 -0700

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints the second half of a string,
+ * followed by a new line.
+ * @str: Pointer to a null-terminated string.
+ */
+void puts_half(char *str)
+{
+	int length = 0;
+	int start;
+
+	while (str[length] != '\0')
+		length++;
+
+	if (length % 2 == 0)
+		start = length / 2;
+	else
+		start = (length + 1) / 2;
+
+	while (str[start] != '\0')
+	{
+		_putchar(str[start]);
+		start++;
+	}
+
+	_putchar('\n');
+}
