8fac8eadfd3256102100565aba2995b815c246d0
agunbiadepaul <agunbiade_paul@yahoo.com>
2023-08-23 12:08:16 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototypes */
+int _putchar(char c);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+
+#endif /* MAIN_H */
