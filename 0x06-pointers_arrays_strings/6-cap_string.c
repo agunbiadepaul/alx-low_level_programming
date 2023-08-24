cc4fdbda282b428bc81642d4183bbb35d82096d5
agunbiadepaul <agunbiade_paul@yahoo.com>
2023-08-23 22:59:55 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalize all words of a string.
+ * @str: The input string.
+ * Return: A pointer to the modified string.
+ */
+char *cap_string(char *str)
+{
+	char *ptr = str;
+	int capitalize_next = 1;
+
+	while (*ptr != '\0')
+	{
+		if (capitalize_next && (*ptr >= 'a' && *ptr <= 'z'))
+		{
+			*ptr = *ptr - 'a' + 'A';
+			capitalize_next = 0;
+		}
+
+		if (*ptr == ' ' || *ptr == '\t' || *ptr == '\n' ||
+			*ptr == ',' || *ptr == ';' || *ptr == '.' ||
+			*ptr == '!' || *ptr == '?' || *ptr == '"' ||
+			*ptr == '(' || *ptr == ')' || *ptr == '{' ||
+			*ptr == '}')
+		{
+			capitalize_next = 1;
+		}
+
+		ptr++;
+	}
+
+	return (str);
+}
