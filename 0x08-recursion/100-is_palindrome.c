b44f4d6f36f1bba7866ed24850f8097a0a28c120
agunbiadepaul <agunbiade_paul@yahoo.com>
2023-08-30 18:24:38 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,14 +8,10 @@
  */
 int _strlen(char *s)
 {
-	int length = 0;
+	if (*s == '\0')
+		return (0);
 
-	while (*s != '\0')
-	{
-		length++;
-		s++;
-	}
-	return (length);
+	return (1 + _strlen(s + 1));
 }
 
 /**
