3250955e980e078ffb1d7d9eb160eb673fa37bb3
agunbiadepaul <agunbiade_paul@yahoo.com>
2023-09-08 13:09:49 -0700

    101-mul.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
 
 /* Function prototypes */
 void *malloc_checked(unsigned int b);
@@ -10,5 +12,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _isdigit(char c);
+char *multiply(char *num1, char *num2);
+void errors(void);
+int _strlen (char *s);
+int is_digit(char *s);
 
 #endif /* MAIN_H */
