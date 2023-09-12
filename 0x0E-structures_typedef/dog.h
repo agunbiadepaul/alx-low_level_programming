06d197588cfe6deb2da8ddb1d97ea9a2c5320b4d
agunbiadepaul <agunbiade_paul@yahoo.com>
2023-09-11 19:43:17 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,8 @@
 #define DOG_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct dog - Define the stuct dog
@@ -24,5 +26,6 @@ typedef struct dog dog_t;
 int main(void);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /* DOG_H */
