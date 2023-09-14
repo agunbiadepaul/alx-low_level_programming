e02545194428c8c19021e0e0a529f064d407e17f
agunbiadepaul <agunbiade_paul@yahoo.com>
2023-09-13 18:33:50 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+#include <stdio.h>
+
+/* Function prototype for print_name function */
+void print_name(char *name, void (*f)(char *));
+void print_name_as_is(char *name);
+void print_name_uppercase(char *name);
+
+#endif /* FUNCTION_POINTERS_H */
