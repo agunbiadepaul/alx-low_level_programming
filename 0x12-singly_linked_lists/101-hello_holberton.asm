0b3b8e14327bd8792f43066d962653d05a836660
agunbiadepaul <agunbiade_paul@yahoo.com>
2023-09-22 15:55:18 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,21 @@
 section .data
-	hello db "Hello, Holberton", 0	; Added '\n' for immediate output
+    hello_message db "Hello, Holberton",10,0  ; The string to be printed with a newline character
+    format db "%s", 0                       ; The format string for printf
 
 section .text
-	global main
-	extern puts
-main:
-	mov rdi, hello	; Load the address of the string into rdi
-	call puts	; Call the puts function
+    global main
 
-	; Exit the program
-	mov rax, 60     ; syscall number for exit (64-bit)
-	xor rdi, rdi    ; Status code 0
-	syscall
+    extern printf
+
+main:
+    ; Calling printf to print the message
+    mov rdi, format        ; Load the format string address into rdi
+    mov rax, 0            ; Clear rax register (no floating-point arguments)
+    lea rsi, [hello_message] ; Load the address of hello_message into rsi
+    call printf           ; Call the printf function
+
+    ; Exit the program
+    mov rax, 60           ; syscall number for exit
+    xor rdi, rdi          ; Exit status 0
+    syscall
+
