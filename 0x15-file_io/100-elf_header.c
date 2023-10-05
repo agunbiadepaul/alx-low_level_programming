35ad3f43c919facc61ff6d690ba0be60da3b8718
agunbiadepaul <agunbiade_paul@yahoo.com>
2023-10-05 10:37:50 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -8,23 +8,37 @@
 
 #define BUF_SIZE 64
 
+/**
+ * print_error - Print error message to stderr and exit with status 98.
+ * @msg: The error message to print.
+ */
 void print_error(const char *msg)
 {
 	fprintf(stderr, "%s\n", msg);
 	exit(98);
 }
 
+/**
+ * read_bytes - Read a specified number of bytes from a file descriptor.
+ * @fd: The file descriptor.
+ * @buf: The buffer to read into.
+ * @count: The number of bytes to read.
+ */
 void read_bytes(int fd, void *buf, size_t count)
 {
 	ssize_t result;
 
 	result = read(fd, buf, count);
-    if (result != (ssize_t)count)
-    {
+	if (result != (ssize_t)count)
+	{
 		print_error("Error reading from file");
 	}
 }
 
+/**
+ * print_elf_header - Print information from the ELF header.
+ * @header: Pointer to the ELF header structure.
+ */
 void print_elf_header(const Elf64_Ehdr *header)
 {
 	int i;
@@ -36,19 +50,29 @@ void print_elf_header(const Elf64_Ehdr *header)
 		printf("%02x ", header->e_ident[i]);
 	}
 	printf("\n");
-	printf("  Class:                             %s\n", header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
-	printf("  Data:                              %s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
-	printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
-	printf("  OS/ABI:                            %s\n", (header->e_ident[EI_OSABI] == ELFOSABI_SYSV) ? "UNIX - System V" : "UNIX - NetBSD");
-	printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
-	printf("  Type:                              %s\n", (header->e_type == ET_EXEC) ? "EXEC (Executable file)" : "DYN (Shared object file)");
-	printf("  Entry point address:               0x%lx\n", (unsigned long)header->e_entry);
+	printf("  Class:		%s\n",
+			header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
+	printf("  Data:			%s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ?
+			"2's complement, little endian" : "2's complement, big endian");
+	printf("  Version:		%d (current)\n", header->e_ident[EI_VERSION]);
+	printf("  OS/ABI:		%s\n", (header->e_ident[EI_OSABI] == ELFOSABI_SYSV) ?
+			"UNIX - System V" : "UNIX - NetBSD");
+	printf("  ABI Version:		%d\n", header->e_ident[EI_ABIVERSION]);
+	printf("  Type:			%s\n", (header->e_type == ET_EXEC) ?
+			"EXEC (Executable file)" : "DYN (Shared object file)");
+	printf("  Entry point address:	0x%lx\n", (unsigned long)header->e_entry);
 }
 
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array containing the command-line arguments.
+ * Return: 0 on success, 98 on error.
+ */
 int main(int argc, char *argv[])
 {
 	const char *filename;
-        int fd;
+	int fd;
 	Elf64_Ehdr header;
 
 	if (argc != 2)
@@ -58,9 +82,11 @@ int main(int argc, char *argv[])
 
 	filename = argv[1];
 	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 	{
-		print_error("Error opening file");
+		perror("Error opening file");
+		exit(98);
 	}
 
 	read_bytes(fd, &header, sizeof(Elf64_Ehdr));
