370c4b42d76416f85b244f383084fad725c161ed
agunbiadepaul <agunbiade_paul@yahoo.com>
2023-10-30 13:08:21 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,38 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <string.h>
 #include "main.h"
 
 /**
- * create_file - Creates a file with permissions and writes content.
- * @filename: The name of the file to create.
- * @text_content: A NULL-terminated string to write to the file.
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
  *
- * Return: 1 on success, -1 on failure.
+ * Return: 1 if it success. -1 if it fails.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, write_result;
+	int fd;
+	int numbletters;
+	int rwr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
+
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		write_result = write(fd, text_content, strlen(text_content));
-		if (write_result == -1)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
+	if (!text_content)
+		text_content = "";
+
+	for (numbletters = 0; text_content[numbletters]; numbletters++)
+		;
+
+	rwr = write(fd, text_content, numbletters);
+
+	if (rwr == -1)
+		return (-1);
 
 	close(fd);
+
 	return (1);
 }
-
