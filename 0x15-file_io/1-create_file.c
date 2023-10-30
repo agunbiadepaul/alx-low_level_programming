32e4e53dea934d3030c77e42226281ed85541a72
agunbiadepaul <agunbiade_paul@yahoo.com>
2023-10-30 13:29:06 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,13 +10,13 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int numbletters;
+	int nletters;
 	int rwr;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
@@ -24,10 +24,10 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	for (numbletters = 0; text_content[numbletters]; numbletters++)
+	for (nletters = 0; text_content[nletters]; nletters++)
 		;
 
-	rwr = write(fd, text_content, numbletters);
+	rwr = write(fd, text_content, nletters);
 
 	if (rwr == -1)
 		return (-1);
