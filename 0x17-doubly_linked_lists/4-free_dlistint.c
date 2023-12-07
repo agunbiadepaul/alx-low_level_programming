512209be1f76d5b56af3983c44c875ed7aca3732
agunbiadepaul <agunbiade_paul@yahoo.com>
2023-12-07 12:47:45 -0800

    4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,11 +1,10 @@
 #include "lists.h"
 
-
-/*
+/**
  * free_dlistint - Frees a linked dlistint_t list.
  * @head: The head of the dlistint_t list.
  *
- * Return: void
+ * Description: This function frees a dlistint_t list.
  */
 void free_dlistint(dlistint_t *head)
 {
