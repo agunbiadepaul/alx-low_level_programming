a1f398246d7ca63b4c2224cbe0629946d8211a1e
agunbiadepaul <agunbiade_paul@yahoo.com>
2023-12-11 20:50:54 -0800

    100-operations.so

---
+++ 0x18-dynamic_libraries/arithemic.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * add - add two integers
+ * @a: first int
+ * @b: second int
+ * Return: addition of integer
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subtract two integers
+ * @a: first int
+ * @b: second int
+ * Return: the subtraction of the integer
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * add - divide two integers
+ * @a: first int
+ * @b: second int
+ * Return: the divisible of the integer
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mul - multiply two integers
+ * @a: first int
+ * @b: second int
+ * Return: multiplication of integer
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * mod - modulus two integers
+ * @a: first int
+ * @b: second int
+ * Return: the remainder of integer
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
